2b1a63bb69cdd143beb08f6310b30c37d0b776f7
opushnev <opushnev@inbox.ru>
2020-09-09 23:03:44 +0300

    road

--- main.js
+++ main.js
@@ -14,16 +14,40 @@ const keys={
 const setting={
     start:false,
     score:0,
-    speed:3
+    speed:3,
+    traffic:3
 };
 const playGame=()=>{
+moveRoad();
     if (setting.start) {
+        if (keys.ArrowLeft && setting.x>0){
+            setting.x-=setting.speed;
+        }
+        if (keys.ArrowRight && setting.x<250){
+            setting.x+=setting.speed;
+        }
+        if (keys.ArrowDown && setting.y<(gameArea.offsetHeight-car.offsetHeight)){
+            setting.y+=setting.speed;
+        }
+        if (keys.ArrowUp && setting.y>0){
+            setting.y-=setting.speed;
+        }
+        car.style.left=setting.x+'px';
+        car.style.top=setting.y+'px';
         requestAnimationFrame(playGame);
     }    
 }
 const startGame=()=>{
     setting.start=true;
-    gameArea.appendChild(car);
+    for (let i = 0; i < 20;i++) {
+      const line=document.createElement('div');
+      line.classList.add('line');
+      line.style.top=(i*75)+'px';
+      line.y=i*75;
+      gameArea.appendChild(line);
+        }
+    gameArea.appendChild(car);setting.x=car.offsetLeft;   
+    setting.y=car.offsetTop;    
     requestAnimationFrame(playGame)
 }
 
@@ -39,3 +63,14 @@ document.addEventListener('click',startGame)
 document.addEventListener('keydown',startRun);
 document.addEventListener('keyup',stopRun);
 
+function moveRoad(){
+    let lines=document.querySelectorAll('.line');
+    lines.forEach(function(item,i){
+item.y+=setting.speed;
+item.style.top=item.y+'px';
+if (item.y>document.documentElement.clientHeight){
+    item.y=-100;
+}
+
+    })
+}
